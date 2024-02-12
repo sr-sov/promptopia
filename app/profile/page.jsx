635a8fe8bff3b601bffb1e924a5be19cd2c21587
Joshua David Sta Rita <jdcstarita@gmail.com>
2024-02-12 15:45:55 +0800

    PromptCard and initial profile page

---
+++ app/profile/page.jsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Profile from '@components/profile';
+
+const Profile = () => {
+  return (
+    <Profile
+        name="My"
+        desc="Welcome to your persionalized profile page"
+        data={[]}
+        handleEdit={}
+        handleDelete={}
+    />
+  )
+}
+
+export default Profile
